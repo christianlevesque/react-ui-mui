c46bf1cc8e8de0fc95a84df6e7324e7b6512beff
Christian LeVesque <christian@levesque.dev>
2025-02-09 19:42:23 -0600

    added Bulma colors

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,25 @@
-﻿import type { OverridableStringUnion } from '@mui/types';
+import type { OverridableStringUnion } from '@mui/types';
 
 export type MuiColor = 'inherit' | 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'error';
+
+/**
+ * Bulma theme colors for use by themeable components
+ */
+export type Color =
+	| 'primary'
+	| 'secondary'
+	| 'tertiary'
+	| 'link'
+	| 'success'
+	| 'info'
+	| 'warning'
+	| 'danger'
+	| 'text'
+	| 'white'
+	| 'light'
+	| 'black'
+	| 'dark'
+	| 'current'
+	| 'inherit';
 
 export type ExtensibleColor<T> = OverridableStringUnion<MuiColor, T>;
