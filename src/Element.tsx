a9e05c29b1d0e484b77ba0443fb36bbe5a317239
Christian LeVesque <christian@levesque.dev>
2025-02-12 12:51:48 -0600

    updated <Element> to not render theme props

--- src/Element.tsx
+++ src/Element.tsx
@@ -1,6 +1,6 @@
 ﻿import type { ElementType, ComponentPropsWithRef, ReactNode } from 'react';
 import { classNames } from '@sienar/react-utils';
-import type { Display, Themeable } from '@/theme.ts';
+import type { Breakpoint, Display, Themeable } from '@/theme.ts';
 
 /**
  * The props for an <code>&lt;HtmlElement&gt;</code>
@@ -10,7 +10,7 @@ export type ElementProps<T extends ElementType> = {
 	children?: ReactNode;
 	className?: string;
 	otherProps?: ComponentPropsWithRef<T>;
-} & Themeable
+} & Themeable;
 
 /**
  * Renders arbitrary HTML or React components
@@ -20,23 +20,23 @@ export default function Element<T extends ElementType>(props: ElementProps<T>) {
 		tag: Tag,
 		children,
 		className,
+		display,
+		srOnly,
 		...otherProps
 	} = props;
 
 	const classes = classNames(
 		{
-			'is-sr-only': props.srOnly
+			'is-sr-only': srOnly
 		},
 		className,
-		...getDisplay(props)
+		...getDisplay(display)
 	);
 
 	return <Tag className={classes} children={children} {...otherProps as any}/>;
 }
 
-function getDisplay(props: Themeable): string[] {
-	const { display } = props;
-
+function getDisplay(display: Partial<Record<Display, Breakpoint>>|undefined): string[] {
 	if (!display) return [];
 
 	const result: string[] = [];
